a9a27a9e49e692c3cfe0e249b6661c0c302d803d
QuePark <gyuhapark11@gmail.com>
2023-05-12 10:48:50 +0900

    Create pages home, contractInfo, notFound and apply to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+  import Router from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
   import Header from "./Components/Header.svelte";
+  import Home from "./pages/Home.svelte";
+  import NotFound from "./pages/NotFound.svelte";
 </script>
 
 <main>
   <Header />
+  <Router routes={{
+    '/': Home,
+    '/contracts/:contractAddress': wrap({
+      asyncComponent: () => import('./pages/ContractInfo.svelte'),
+    }),
+    '*': NotFound
+  }} />
 </main>
 
 <style lang="postcss">
---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div>
+    404 Not Found
+</div>
