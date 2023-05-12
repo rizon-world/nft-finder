91e4c74d6a8b5f27bf3b34bb57327568e6ca90f9
QuePark <gyuhapark11@gmail.com>
2023-05-12 14:14:27 +0900

    Change the contract route to the contract address or change it to home

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -2,7 +2,7 @@
   import NetworkHandler from "./NetworkHandler.svelte";
   import logo from '../assets/rizon_logo.png';
   import contractAddress from "../stores/contractAddress";
-  import { push } from "svelte-spa-router";
+  import { push, replace } from "svelte-spa-router";
 
   let focused = false;
 
@@ -18,10 +18,13 @@
   function handleSearch(e: Event) {
     e.preventDefault();
     contractAddress.subscribe((value) => {
-      push(`/contract/${value}`);
+      if (value) {
+        push(`/contract/${value}`);
+      } else {
+        replace("/");
+      }
     });
   }
-
 </script>
 
 <header class="sticky top-0 bg-white">
@@ -29,14 +32,14 @@
       <div>
         <img src={logo} alt="Svelte logo" class=" w-16 mx-2" />
       </div>
-      <form action="/contract/" class={`w-1/2 mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 md:flex hidden ${focused ? "focus-on" : ""}`} on:submit={handleSearch} >
+      <form class={`w-1/2 mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 md:flex hidden ${focused ? "focus-on" : ""}`} on:submit={handleSearch} >
           <input type="text" name="search" class="w-full mx-auto focus-none" placeholder="Enter the contract address" on:focusin={handleFocus} on:focusout={handleFocus} on:change={handleChange} />
           <button class="my-auto px-1"><i class="fa-solid fa-magnifying-glass"></i></button>
       </form>
       <NetworkHandler />
     </div>
-    <div class="text-center p-4 mx-0">
-      <form class={`w-full mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 md:hidden flex ${focused ? "focus-on" : ""}`} on:submit={(e) => e.preventDefault()} >
+    <div class="text-center p-4 mx-0 md:hidden">
+      <form class={`w-full mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 flex ${focused ? "focus-on" : ""}`} on:submit={(e) => e.preventDefault()} >
         <input type="text" class="w-full mx-auto focus-none" placeholder="Enter the contract address"  on:focusin={handleFocus} on:focusout={handleFocus} on:change={handleChange} />
         <button class="my-auto px-1"><i class="fa-solid fa-magnifying-glass"></i></button>
       </form>
