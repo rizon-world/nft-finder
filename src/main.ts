e461ca8405005976f6b6f044fff38503a46267a8
QuePark <gyuhapark11@gmail.com>
2023-05-12 16:52:19 +0900

    Add module axios and buffer: set up polyfill for buffer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import './hmr';
 import './main.css';
-import App from './App.svelte'
+import App from './App.svelte';
+import { Buffer } from 'buffer';
+
+globalThis.Buffer = Buffer;
 
 const app = new App({
-  target: document.body
-})
+  target: document.body,
+});
 
-export default app
+export default app;
