8b765707dfb75545bd011d79dc4480692d3868c7
QuePark <gyuhapark11@gmail.com>
2023-05-12 11:01:32 +0900

    Push browser history for spa

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -2,6 +2,7 @@
   import NetworkHandler from "./NetworkHandler.svelte";
   import logo from '../assets/rizon_logo.png';
   import contractAddress from "../stores/contractAddress";
+  import { push } from "svelte-spa-router";
 
   let focused = false;
 
@@ -13,6 +14,14 @@
     const { value } = e.target as HTMLSelectElement;
     contractAddress.set(value);
   }
+
+  function handleSearch(e: Event) {
+    e.preventDefault();
+    contractAddress.subscribe((value) => {
+      push(`/contracts/${value}`);
+    });
+  }
+
 </script>
 
 <header class="sticky top-0 bg-white">
@@ -20,7 +29,7 @@
       <div>
         <img src={logo} alt="Svelte logo" class=" w-16 mx-2" />
       </div>
-      <form action="/contracts/" class={`w-1/2 mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 md:flex hidden ${focused ? "focus-on" : ""}`} on:submit={(e) => e.preventDefault()} >
+      <form action="/contracts/" class={`w-1/2 mx-auto justify-between p-2 my-2 flex-shrink rounded-md  border-solid border-2 border-gray-600 md:flex hidden ${focused ? "focus-on" : ""}`} on:submit={handleSearch} >
           <input type="text" name="search" class="w-full mx-auto focus-none" placeholder="Enter the contract address" on:focusin={handleFocus} on:focusout={handleFocus} on:change={handleChange} />
           <button class="my-auto px-1"><i class="fa-solid fa-magnifying-glass"></i></button>
       </form>
