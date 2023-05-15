e5c04dd219702b5f4d84f183d8e3627def4a60f7
QuePark <gyuhapark11@gmail.com>
2023-05-15 10:37:28 +0900

    Update prettier options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,15 @@
 module.exports = {
-    trailingComma: 'all',
-    tabWidth: 2,
-    singleQuote: true,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: true,
+  svelteBracketNewLine: false,
+  svelteAllowShorthand: false,
+  svelteIndentScriptAndStyle: false,
+  plugins: [
+    'prettier-plugin-svelte',
+    'prettier-plugin-tailwindcss', // MUST come last
+  ],
+  pluginSearchDirs: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  singleQuote: true,
 };
