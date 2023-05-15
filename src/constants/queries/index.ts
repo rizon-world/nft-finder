8c43974ea870c9b1b3e7acac23982a7ac252e972
QuePark <gyuhapark11@gmail.com>
2023-05-15 10:38:30 +0900

    Add query msg to get minter

--- src/constants/queries/index.ts
+++ src/constants/queries/index.ts
@@ -1,8 +1,10 @@
 import { stoba } from '../../utils';
 import contractInfo from './contractInfo.json';
 import numTokens from './numTokens.json';
+import minter from './minter.json';
 
 export const inputQueries = {
   contractInfo: stoba(JSON.stringify(contractInfo)),
   numTokens: stoba(JSON.stringify(numTokens)),
+  minter: stoba(JSON.stringify(minter)),
 };
