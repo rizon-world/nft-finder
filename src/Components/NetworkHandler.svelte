d740fbe1b26481dce158b2a3687e25eeb314075f
QuePark <gyuhapark11@gmail.com>
2023-05-16 10:57:08 +0900

    Add localstorage to remember network

--- src/Components/NetworkHandler.svelte
+++ src/Components/NetworkHandler.svelte
@@ -6,11 +6,21 @@ let networks = ['mainnet', 'testnet'];
 let value = writable('mainnet');
 const { isMainnet } = networkConfig;
 
+const storageNetwork = localStorage.getItem('network');
+if (storageNetwork) {
+  value.set(storageNetwork);
+} else {
+  value.set('mainnet');
+  localStorage.setItem('network', 'mainnet');
+}
+
 value.subscribe((val) => {
   if (val === 'mainnet') {
     isMainnet.set(true);
+    localStorage.setItem('network', 'mainnet');
   } else {
     isMainnet.set(false);
+    localStorage.setItem('network', 'testnet');
   }
 });
 </script>
