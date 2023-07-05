2116a79ec0992a25fecbfcadadaa8c5b97c8ac3f
QuePark <gyuhapark11@gmail.com>
2023-07-05 14:54:35 +0900

    🐛 Fix a bug: handle a base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nft-finder/',
   plugins: [svelte(), nodePolyfills()],
 });
