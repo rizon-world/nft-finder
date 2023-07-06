20d6da212f09a5b4c064ec51e3c0b7026947b6dc
QuePark <gyuhapark11@gmail.com>
2023-07-06 14:53:46 +0900

    🚑 Connect cname and remove base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nft-finder/',
   plugins: [svelte(), nodePolyfills()],
 });
